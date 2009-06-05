b27809acf2dfadb4d8415473d2e5860963259112
Joannes Vermorel <joannes.vermorel@lokad.com>
2009-06-05 16:23:48 +0000

    Test project for the framework added.

---
+++ Test/Lokad.Cloud.Framework.Test/BlobSetTests.cs
@@ -0,0 +1,32 @@
+﻿#region Copyright (c) Lokad 2009
+// This code is released under the terms of the new BSD licence.
+// URL: http://www.lokad.com/
+#endregion
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using NUnit.Framework;
+
+namespace Lokad.Cloud.Framework.Test
+{
+	[TestFixture]
+	public class BlobSetTests
+	{
+		// validating the anonymous methods could be serialized
+		[Test]
+		public void FuncSerialization()
+		{
+			var myFunc = new Func<int, int>(i => i*i);
+
+			var stream = new MemoryStream();
+			var formatter = new BinaryFormatter();
+
+			formatter.Serialize(stream, myFunc);
+
+			stream.Position = 0;
+			var myFuncBis = (Func<int, int>) formatter.Deserialize(stream);
+
+			Assert.AreEqual(myFunc(5), myFuncBis(5), "#A00");
+		}
+	}
+}
---
+++ Test/Lokad.Cloud.Framework.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lokad.Cloud.Framework.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lokad.com")]
+[assembly: AssemblyProduct("Lokad.Cloud.Framework.Test")]
+[assembly: AssemblyCopyright("Copyright © Lokad 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8a3d64f3-fa8f-4936-971f-c41712a8310e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
