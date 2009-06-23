f60e6fb2d4fa5b80073c42284c0124df8939e0fe
Joannes Vermorel <joannes.vermorel@lokad.com>
2009-06-23 13:51:17 +0000

    Adding missing files (ZIP library + sample DLL).

---
+++ Test/Lokad.Cloud.Core.Test/Sample/ReadMe.txt
@@ -0,0 +1,5 @@
+This sample DLL is used to test dynamic assembly loading with Lokad.Cloud.
+
+DLL can be regenerated from the compiler command line with:
+
+csc /out:sample.dll /t:library sample.cs
---
+++ Test/Lokad.Cloud.Core.Test/Sample/sample.cs
@@ -0,0 +1,12 @@
+// compile me with
+// csc /out:sample.dll /t:library sample.cs
+namespace Lokad.Example
+{
+	public class MyClass
+	{
+		public string MyMethod()
+		{
+			return "foo";
+		}
+	}
+}
