2ec3c623d624488614589eb4e213493c6245ddc3
Dario Solera <dario.solera@lokad.com>
2009-06-26 13:37:51 +0000

    Added missing file.

---
+++ Test/Lokad.Cloud.Framework.Test/CuidTests.cs
@@ -0,0 +1,20 @@
+﻿#region Copyright (c) Lokad 2009
+// This code is released under the terms of the new BSD licence.
+// URL: http://www.lokad.com/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Lokad.Cloud.Core;
+
+namespace Lokad.Cloud.Framework.Test
+{
+	[TestFixture]
+	public class CuidTests
+	{
+
+	}
+
+}
