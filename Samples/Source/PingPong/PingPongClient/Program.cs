866d49fb8bcca2683720d790552aae02044398c5
Joannes Vermorel <joannes.vermorel@lokad.com>
2009-07-24 16:16:22 +0000

    Updating the sample with merged library.

--- Samples/Source/PingPong/PingPongClient/Program.cs
+++ Samples/Source/PingPong/PingPongClient/Program.cs
@@ -3,9 +3,12 @@
 // URL: http://www.lokad.com/
 #endregion
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Autofac.Builder;
+using Autofac.Configuration;
+using Lokad;
+using Lokad.Cloud.Azure;
+using Lokad.Cloud.Core;
+using ILog=log4net.ILog;
 
 namespace PingPongClient
 {
@@ -13,6 +16,32 @@ namespace PingPongClient
 	{
 		static void Main(string[] args)
 		{
+			// TODO: no logic implemented yet
+		}
+
+		static Autofac.IContainer Setup()
+		{
+			var builder = new ContainerBuilder();
+			builder.RegisterModule(new ConfigurationSettingsReader("autofac"));
+
+			var policy = ActionPolicy
+				.With(HandleException)
+				.Retry(10, (e, i) => SystemUtil.Sleep(5.Seconds()));
+
+			builder.Register(policy);
+
+			builder.Register(c => (ITypeMapperProvider)new TypeMapperProvider());
+			builder.Register(c => (ILog)new CloudLogger(c.Resolve<IBlobStorageProvider>()));
+
+			return builder.Build();
+		}
+
+		static bool HandleException(Exception ex)
+		{
+			//if (ex is StorageServerException)
+			//	return true;
+
+			return false;
 		}
 	}
 }
