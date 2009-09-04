3b7204a3b8f13963a0d59d798d7568e17be5ed79
Matthieu Durut <matthieu.durut@lokad.com>
2009-09-04 11:26:45 +0000

    Adding mock CloudLogger

---
+++ Source/Lokad.Cloud.Framework/Mock/MemoryLogger.cs
@@ -0,0 +1,30 @@
+﻿#region Copyright (c) Lokad 2009
+// This code is released under the terms of the new BSD licence.
+// URL: http://www.lokad.com/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lokad.Cloud.Mock
+{
+	class MemoryLogger : ILog
+	{
+		public bool IsEnabled(LogLevel level)
+		{
+			return false;
+		}
+
+		public void Log(LogLevel level, Exception ex, object message)
+		{
+			//do nothing
+		}
+
+		public void Log(LogLevel level, object message)
+		{
+			Log(level, null, message);
+		}
+	}
+}
