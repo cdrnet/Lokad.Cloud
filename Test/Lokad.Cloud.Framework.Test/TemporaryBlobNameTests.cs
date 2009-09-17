d1b1b8fe61db95c37b622cea85f70f0098b33365
Matthieu Durut <matthieu.durut@lokad.com>
2009-09-17 08:18:20 +0000

    Adding new Unit Tests on temporaryBlobNames.

---
+++ Test/Lokad.Cloud.Framework.Test/TemporaryBlobNameTests.cs
@@ -0,0 +1,27 @@
+﻿#region Copyright (c) Lokad 2009
+// This code is released under the terms of the new BSD licence.
+// URL: http://www.lokad.com/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Lokad.Cloud.Test
+{
+	[TestFixture]
+	public class TemporaryBlobNameTests
+	{
+		[Test]
+		public void TemporaryBlobNamesAreUnique()
+		{
+			var expiration = new DateTime(2100, 12, 31);
+			var firstBlobName = TemporaryBlobName.GetNew(expiration);
+			var secondBlobName = TemporaryBlobName.GetNew(expiration);
+
+			Assert.AreNotEqual(firstBlobName.Suffix, secondBlobName.Suffix, "two different temporaryBlobNames should have different suffix");
+		}
+	}
+}
