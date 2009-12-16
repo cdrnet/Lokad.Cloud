189d3a9eb7cf3444a430b6a074672ee590fc894f
Joannes Vermorel <joannes.vermorel@lokad.com>
2009-12-16 13:41:50 +0000

    Replacing serialization pattern by [DataContract] (most resilient to versionning).

--- Source/Lokad.Cloud.Framework/MessageWrapper.cs
+++ Source/Lokad.Cloud.Framework/MessageWrapper.cs
@@ -3,7 +3,6 @@
 // URL: http://www.lokad.com/
 #endregion
 
-using System;
 using System.Runtime.Serialization;
 
 namespace Lokad.Cloud
@@ -11,30 +10,13 @@ namespace Lokad.Cloud
 	/// <summary>The purpose of the <see cref="MessageWrapper"/> is to gracefully
 	/// handle messages that are too large of the queue storage (or messages that 
 	/// happen to be already stored in the Blob Storage).</summary>
-	[Serializable]
-	internal sealed class MessageWrapper : ISerializable
+	[DataContract]
+	internal sealed class MessageWrapper
 	{
+		[DataMember]
 		public string ContainerName { get; set; }
 
+		[DataMember]
 		public string BlobName { get; set; }
-
-		public MessageWrapper()
-		{
-		}
-
-		/// <summary>Deserialization constructor.</summary>
-		public MessageWrapper(SerializationInfo info, StreamingContext context)
-		{
-			ContainerName = info.GetString("cn");
-			BlobName = info.GetString("bn");
-		}
-
-		/// <summary>Serialization method.</summary>
-		public void GetObjectData(SerializationInfo info, StreamingContext context)
-		{
-			info.AddValue("vs", "1.0");
-			info.AddValue("cn", ContainerName);
-			info.AddValue("bn", BlobName);
-		}
 	}
 }
