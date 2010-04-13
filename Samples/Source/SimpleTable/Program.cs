f3f20fcf51c4ccab7e959c2830d6eef797f3c07a
Joannes Vermorel <joannes.vermorel@lokad.com>
2010-04-13 11:56:21 +0000

    New simple example illustrating Table Storage with stand-alone setup.

---
+++ Samples/Source/SimpleTable/Program.cs
@@ -0,0 +1,53 @@
+﻿#region Copyright (c) Lokad 2010
+// This code is released under the terms of the new BSD licence.
+// URL: http://www.lokad.com/
+#endregion
+using System;
+using System.Runtime.Serialization;
+using Lokad.Cloud;
+using Lokad.Cloud.Storage;
+
+namespace SimpleTable
+{
+    [DataContract]
+    class Book
+    {
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Author { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: change your connection string here
+            var providers = Standalone.CreateProviders("DefaultEndpointsProtocol=https;AccountName=;AccountKey=");
+            
+            // 'books' is the name of the table
+            var books = new CloudTable<Book>(providers.TableStorage, "books");
+
+            var potterBook = new Book { Author = "J. K. Rowling", Title = "Harry Potter" };
+            var poemsBook = new Book { Author = "John Keats", Title = "Complete Poems" };
+
+            // inserting (or updating record in Table Storage)
+            books.Upsert(new[]
+                {
+                    new CloudEntity<Book> {PartitionKey = "UK", RowRey = "potter", Value = potterBook},
+                    new CloudEntity<Book> {PartitionKey = "UK", RowRey = "poems", Value = poemsBook}
+                });
+
+            // reading from table
+            foreach(var entity in books.Get())
+            {
+                Console.WriteLine("{0} by {1} in partition '{2}' and rowkey '{3}'",
+                    entity.Value.Title, entity.Value.Author, entity.PartitionKey, entity.RowRey);
+            }
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
