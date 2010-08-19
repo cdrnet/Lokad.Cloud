819e5fb71d3cbe639c6c00c34ce01541b9235e57
Christoph Ruegg <christoph.ruegg@lokad.com>
2010-08-19 16:31:32 +0000

    Cloud.Storage: slowly beginning to merge over to Lokad.Cloud solution

---
+++ Source/Lokad.Cloud.Storage/Documents/IDocumentStorage.cs
@@ -0,0 +1,11 @@
+﻿#region Copyright (c) Lokad 2009-2010
+// This code is released under the terms of the new BSD licence.
+// URL: http://www.lokad.com/
+#endregion
+
+namespace Lokad.Cloud.Storage
+{
+	public interface IDocumentStorage
+	{
+	}
+}
